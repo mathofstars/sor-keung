d919f6a75caaddf4303eac3f1c9adef5be614216
mathofstars <mathofstars@gmail.com>
2026-09-25 15:26:03 +0100

    feat: safely render assistant Markdown with allowlisted HTML

---
+++ desktop/markdown.ts
@@ -0,0 +1,57 @@
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { toHast } from "mdast-util-to-hast";
+import { sanitize, type Schema } from "hast-util-sanitize";
+import { toHtml } from "hast-util-to-html";
+
+const SAFE_MARKDOWN_SCHEMA: Schema = {
+  tagNames: [
+    "p",
+    "strong",
+    "em",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "ul",
+    "ol",
+    "li",
+    "code",
+    "pre",
+    "blockquote",
+    "br"
+  ],
+  attributes: {}
+};
+
+export function markdownToSafeHtml(markdown: string): string {
+  const markdownTree = fromMarkdown(markdown);
+  const htmlTree = toHast(markdownTree, {
+    allowDangerousHtml: false
+  });
+
+  if (!htmlTree) {
+    return "";
+  }
+
+  const safeTree = sanitize(htmlTree, SAFE_MARKDOWN_SCHEMA);
+  return toHtml(safeTree, {
+    allowDangerousHtml: false
+  });
+}
+
+export function renderAssistantMarkdown(
+  element: HTMLElement,
+  markdown: string
+): void {
+  const safeHtml = markdownToSafeHtml(markdown);
+  const parsed = new DOMParser().parseFromString(safeHtml, "text/html");
+  const fragment = document.createDocumentFragment();
+
+  for (const node of Array.from(parsed.body.childNodes)) {
+    fragment.append(document.importNode(node, true));
+  }
+
+  element.replaceChildren(fragment);
+}
